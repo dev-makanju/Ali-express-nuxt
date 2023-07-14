2068eafe3ee6f7d2970673f07f02774f6c2be63d
miracool <makurseme@gmail.com>
2023-07-14 08:56:44 +0100

    feat: setup config for folder

---
+++ nuxt.config.ts
@@ -0,0 +1,28 @@
+import { RuntimeConfig } from 'nuxt/schema';
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  pages: true,
+  modules: [
+    "nuxt-icon",
+    "nuxt-lodash",
+    "@nuxtjs/supabase",
+    "@nuxtjs/tailwindcss",
+    "@pinia-plugin-persistedstate/nuxt",
+    "@pinia/nuxt",
+  ],
+  RuntimeConfig: {
+    public: {
+      stripePK: process.env.STRIPE_PK_KEY,
+    }
+  },
+  app: {
+    head: {
+      script: [
+        { 
+          src: 'https://js.stripe.com/v3/', 
+          defer: true 
+        },
+      ]
+    }
+  }
+})
