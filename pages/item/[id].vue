634611ae6516edf6b286b3c653b2cbfa7a852b13
miracool <makurseme@gmail.com>
2023-07-17 19:51:46 +0100

    feat: added new pages

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -23,6 +23,62 @@
             </div>
 
             <div class="md:w-[60%] bg-white p-3 rounded-lg">
+               <div v-if="true">
+                  <p class="mb-2">Title</p>
+                  <p class="font-light text-[12px] mb-2">Description section</p>
+               </div>
+
+               <div class="flex items-center pt-1.5">
+                  <span class="h-4 min-w-4 rounded-full p-0.5 bg-[#FFD000] mr-2">
+                        <Icon name="material-symbols:star-rounded" class="-mt-[17px]" size="12"/>
+                  </span>
+                  <p class="text-[#FF5353]">Extra 5% off</p>
+               </div>
+
+               <div class="flex items-center justify-start my-2">
+                  <Icon name="ic:baseline-star" color="#FF5353"/>
+                  <Icon name="ic:baseline-star" color="#FF5353"/>
+                  <Icon name="ic:baseline-star" color="#FF5353"/>
+                  <Icon name="ic:baseline-star" color="#FF5353"/>
+                  <Icon name="ic:baseline-star" color="#FF5353"/>
+                  <span class="text-[13px] font-light ml-2">5 213 Reviews 1,000+ orders</span>
+               </div>
+               
+               <div class="border-b" />
+
+               <div class="flex items-center justify-start gap-2 my-2">
+                  <div class="text-xl font-bold">$ {{ priceComputed }}</div>
+                  <span class="bg-[#F5F5F5] border text-[#C08562] text-[9px] font-semibold px-1.5 rounded-sm">70% off</span>
+               </div>
+
+               <p class="text-[#009A66] text-xs font-semibold pt-1">
+                  Free 11-day delivery over ￡8.28
+               </p>
+
+               <p class="text-[#009A66] text-xs font-semibold pt-1">
+                  Free Shipping
+               </p>
+
+               <div class="py-2"/>
+
+               <button 
+                     @click="addToCart()"
+                     :disabled="isInCart"
+                     class="
+                     px-6 
+                     py-2 
+                     rounded-lg 
+                     text-white 
+                     text-lg 
+                     font-semibold 
+                     bg-gradient-to-r 
+                     from-[#FF851A] 
+                     to-[#FFAC2C]
+                     "
+               >
+                  <div v-if="isInCart">Is Added</div>
+                  <div v-else>Add to Cart</div>
+               </button>
 
             </div>
          </div>
@@ -33,7 +89,10 @@
 <script setup>
 
 import MainLayout from "~/layouts/MainLayout.vue";
+import { useUserStore } from "~/store/user";
+const route = useRoute();
 
+const userStore = useUserStore();
 let currentImage = ref(null);
 
 onMounted(() => {   
@@ -41,6 +100,20 @@ onMounted(() => {
       currentImage.value = "https://picsum.photos/id/77/800/800"
       images.value[0] = "https://picsum.photos/id/77/800/800";
    })
+})
+
+const isInCart = computed(() => {
+   let res = false
+   userStore.cart.forEach(item => {
+      if(route.params.id == item.id){
+         res = true;
+      }
+   }) 
+   return res;
+})
+
+const priceComputed = computed( () => {
+   return "26.40"
 })
 
 const images = ref([
@@ -51,5 +124,9 @@ const images = ref([
    'https://picsum.photos/id/99/800/800',
    'https://picsum.photos/id/144/800/800',
 ])
+
+const addToCart = (() => {
+   alert("ADDED");
+})
 
 </script>
