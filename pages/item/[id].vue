06da85e1a34079be60e52ed82439b96d1a8bf99b
miracool <makurseme@gmail.com>
2023-07-19 19:29:17 +0100

    feat: integrate product detail endpoint with frontend

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -23,9 +23,9 @@
             </div>
 
             <div class="md:w-[60%] bg-white p-3 rounded-lg">
-               <div v-if="true">
-                  <p class="mb-2">Title</p>
-                  <p class="font-light text-[12px] mb-2">Description section</p>
+               <div v-if="product && product.data">
+                  <p class="mb-2">{{ product.data.title }}</p>
+                  <p class="font-light text-[12px] mb-2">{{ product.data.description }}</p>
                </div>
 
                <div class="flex items-center pt-1.5">
@@ -93,13 +93,20 @@ import { useUserStore } from "~/store/user";
 const route = useRoute();
 
 const userStore = useUserStore();
+let product = ref(null);
 let currentImage = ref(null);
 
-onMounted(() => {   
-   watchEffect( () => {
-      currentImage.value = "https://picsum.photos/id/77/800/800"
-      images.value[0] = "https://picsum.photos/id/77/800/800";
-   })
+onBeforeMount( async () => {
+   product.value = await useFetch(`/api/prisma/get-product-by-id/${route.params.id}`)
+})
+
+
+watchEffect( () => {
+   if (product.value && product.value.data) {
+     currentImage.value = product.value.data.url;
+     images.value[0] = product.value.data.url;
+     userStore.isLoading = false; 
+   }
 })
 
 const isInCart = computed(() => {
@@ -113,7 +120,10 @@ const isInCart = computed(() => {
 })
 
 const priceComputed = computed( () => {
-   return "26.40"
+   if (product.value && product.value.data) {
+     return product.value.data.price / 100;
+   }
+   return "0.00"
 })
 
 const images = ref([
@@ -126,7 +136,7 @@ const images = ref([
 ])
 
 const addToCart = (() => {
-   alert("ADDED");
+   userStore.cart.push(product.value.data);
 })
 
 </script>
